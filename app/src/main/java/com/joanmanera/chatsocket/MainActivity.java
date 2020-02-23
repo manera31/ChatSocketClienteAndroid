125cb73c856a1909656f2c1a3a2d83ba2022a1eb
Joan Manera <joanet31@hotmail.com>
2020-02-23 17:06:25 +0100

    asd

--- app/src/main/java/com/joanmanera/chatsocket/MainActivity.java
+++ app/src/main/java/com/joanmanera/chatsocket/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements MensajePrivadoDia
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Crea una conexión con el servidor para decirle que se acaba de conectar.
         Start tarea1 = new Start();
         tarea1.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
@@ -57,25 +58,35 @@ public class MainActivity extends AppCompatActivity implements MensajePrivadoDia
         etNick = findViewById(R.id.etNick);
         etMensaje = findViewById(R.id.etMensaje);
         bEnviar = findViewById(R.id.bEnviar);
+
+        // Controla el evento del click del boton enviar.
         bEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Cliente tarea = new Cliente();
                 Mensaje m;
+
                 if(isPrivate){
+                    // Si el mensaje es privado construye el mensaje con la IP del destino.
                     m = new Mensaje(etNick.getText().toString(), ipDestino, etMensaje.getText().toString(), isPrivate);
                     isPrivate = false;
                     tvDestino.setText("All");
                 } else {
-                    m = new Mensaje(etNick.getText().toString(), "", etMensaje.getText().toString(), isPrivate);
+                    // Si no es privado construye el mesaje con un destinatario en blanco.
+                    // El servidor entiende que es para todos.
+                    m = new Mensaje(etNick.getText().toString(), "todos", etMensaje.getText().toString(), isPrivate);
                 }
+
+                // Se crea la conexión al servidor pasandole el mensaje.
                 tarea.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, m);
             }
         });
 
+        // Se crea una conexión al servidor donde se recibiran todos los mensajes.
         RecibirMensaje recibirMensaje = new RecibirMensaje();
         recibirMensaje.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
+        // Se muestran los mensajes
         mensajes = new ArrayList<>();
         adapterMensaje = new AdapterMensaje(mensajes);
         recyclerView = findViewById(R.id.recyclerView);
